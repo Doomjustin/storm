0a34d3a36b68636f8325ef7058e7c3ceb205eee0
Doomjustin <1075101233doom@gmail.com>
2025-04-16 22:26:44 +0900

    [add]尝试输出到fmt

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,36 @@
+#include <Eigen/Eigen>
 #include <fmt/base.h>
+#include <fmt/format.h>
 
 #include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+namespace Eigen {
+
+auto format_as(const Matrix2d& matrix)
+{
+    std::stringstream ss{};
+    ss << matrix;
+    return ss.str();
+}
+
+}
+
 
 int main(int argc, char* argv[])
 {
-    fmt::print("Hello, {}!\n", "world");
+    Eigen::Matrix2d m;
+    m << 1, 2, 3, 4;
+
+    fmt::println("{}", m);
+
+    std::cout << m << '\n';
+
+    std::stringstream ss{};
+    ss << m;
+
+    std::cout << ss.str() << '\n';
 
     return EXIT_SUCCESS;
 }
