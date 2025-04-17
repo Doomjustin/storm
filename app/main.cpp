7a86a3d6714b98941b188c8f75d7d92c423563b1
Doomjustin <1075101233doom@gmail.com>
2025-04-17 15:14:45 +0900

    [add]import nlohmann::json

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,11 @@
 #include <Eigen/Eigen>
 #include <fmt/base.h>
 #include <fmt/format.h>
+#include <nlohmann/json.hpp>
 
 #include <cstdlib>
 #include <sstream>
+#include <fstream>
 
 namespace Eigen {
 
@@ -24,11 +26,27 @@ auto format_as(const MatrixXd& matrix)
 
 } // namespace Eigen
 
+
+template<typename T>
+T get_value(const nlohmann::json& j, const std::string& key)
+{
+    if (j.contains(key)) {
+        return j[key].template get<T>();
+    } else {
+        throw std::runtime_error("Key not found in JSON object.");
+    }
+}
+
 int main(int argc, char* argv[])
 {
     using namespace Eigen;
+    using json = nlohmann::json;
     auto m = Eigen::MatrixXd::Random(3, 3);
     fmt::print("Matrix:\n{}\n", m);
+
+    std::fstream file{ "example.json" };
+    auto data = json::parse(file);
+    fmt::println("pi: {}", get_value<double>(data, "pi"));
 
     return EXIT_SUCCESS;
 }
